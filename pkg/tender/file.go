c5feb8c6c3fed0d6ce081de627b1bd7d34f08854
Cody <cody@cravencode.com>
2018-10-02 23:23:56 -0700

    Improve comments for file tender

--- pkg/tender/file.go
+++ pkg/tender/file.go
@@ -2,9 +2,10 @@ package tender
 
 import "net/http"
 
+// File is a single file serving Tender.
 type File struct{}
 
-// DeployTender for File struct attaches a single .
+// DeployTender for File struct attaches a single route to serve a single file.
 func (f *File) DeployTender(path, route string) error {
 	http.HandleFunc(route, func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, path)
